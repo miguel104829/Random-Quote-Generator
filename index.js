edad9cd98ebc3ccab6c4b8886b728d946607cee1
miguel <miguel_samaca@hotmail.com>
2023-03-24 12:08:31 -0500

    se agrega el proyecto

---
+++ index.js
@@ -0,0 +1,37 @@
+const twitterButton = document.querySelector('#js-tweet');
+const spinner = document.querySelector('#js-spinner');
+const newQuoteButton = document.querySelector('#js-new-quote');
+newQuoteButton.addEventListener('click', getQuote);
+
+const endpoint = 'https://api.whatdoestrumpthink.com/api/v1/quotes/random';
+
+async function getQuote() {
+  spinner.classList.remove('hidden');
+  newQuoteButton.disabled = true;
+
+  try {
+    const response = await fetch(endpoint)
+    if (!response.ok) {
+      throw Error(response.statusText);
+    }
+    const json = await response.json();
+    displayQuote(json.message);
+		setTweetButton(json.message);
+  } catch {
+    alert('Failed to fetch new quote');
+  } finally {
+    newQuoteButton.disabled = false;
+    spinner.classList.add('hidden');
+  }
+}
+
+function displayQuote(quote) {
+  const quoteText = document.querySelector('#js-quote-text');
+  quoteText.textContent = quote;
+}
+
+function setTweetButton(quote) {
+  twitterButton.setAttribute('href', `https://twitter.com/share?text=${quote} - Donald Trump`);
+}
+
+getQuote();
